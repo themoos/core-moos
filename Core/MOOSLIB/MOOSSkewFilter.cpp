0f8332cbc34968de3642020be50f874da379f674
arh <arh@5f621d73-a7ea-4b79-bc62-7e07601040e3>
2008-09-10 09:59:18 +0000

    Merged from branch MOOSLibSkewFix Rev 2129 - 2133
    git-svn-id: svn+ssh://login2/data/avl11/svnroot/MOOS/trunk@2135 5f621d73-a7ea-4b79-bc62-7e07601040e3

---
+++ Core/MOOSLIB/MOOSSkewFilter.cpp
@@ -0,0 +1,269 @@
+///////////////////////////////////////////////////////////////////////////
+//
+//   MOOS - Mission Oriented Operating Suite 
+//  
+//   A suit of Applications and Libraries for Mobile Robotics Research 
+//   Copyright (C) 2001-2005 Massachusetts Institute of Technology and 
+//   Oxford University. 
+//	
+//   This software was written by Paul Newman at MIT 2001-2002 and Oxford 
+//   University 2003-2005. email: pnewman@robots.ox.ac.uk. 
+//	  
+//   This file is part of a  MOOS Core Component. 
+//		
+//   This program is free software; you can redistribute it and/or 
+//   modify it under the terms of the GNU General Public License as 
+//   published by the Free Software Foundation; either version 2 of the 
+//   License, or (at your option) any later version. 
+//		  
+//   This program is distributed in the hope that it will be useful, 
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+//   General Public License for more details. 
+//			
+//   You should have received a copy of the GNU General Public License 
+//   along with this program; if not, write to the Free Software 
+//   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 
+//   02111-1307, USA. 
+//
+//////////////////////////    END_GPL    //////////////////////////////////
+
+#include "MOOSSkewFilter.h"
+#include <cmath>
+
+namespace MOOS
+{
+
+
+	CMOOSSkewFilter::CMOOSSkewFilter()
+	{
+		Reset();
+	}
+
+
+	void CMOOSSkewFilter::Reset()
+	{
+		m_bIsRunning   = false;
+		m_dfLastVal    = 0.0;
+		m_dfLastTime   = 0.0;
+		m_env.Reset();
+	}
+	
+
+	double CMOOSSkewFilter::SmoothingFilter(double dfDT, double dfOldFilterVal, double dfNewMeas, double dfGradient) const
+	{
+		// UP filter is much faster than down filter
+		const double alpha_up = 0.01;
+		const double alpha_dn = 0.0001;
+		
+		double pred = dfOldFilterVal + dfGradient * dfDT;
+
+		double innov = dfNewMeas - pred;
+
+		if (innov > 0)
+		{
+			return pred + alpha_up * innov;
+		}
+		else
+		{
+			return pred + alpha_dn * innov;
+		}
+	}
+
+
+	double CMOOSSkewFilter::Update(double dfTXtime, double dfRXtime, double dfTransportDelay)
+	{
+		double dfSkew = dfTXtime - dfRXtime;
+		
+		if (!m_env.AddPoint(dfTXtime, dfSkew))
+		{
+			// Probably has happened because we've got the same time
+			// stamp as the previous measurement.  But could be because
+			// something's gone more wrong with the convex envelope
+			// Until we get better error info from AddPoint
+			// we'll just have to assume the worst
+			m_env.Reset();
+		}
+
+		// Get current best estimate of drift rate and offset
+		double dfGradient = 0.0;
+		CConvexEnvelope::tSeg seg;
+		if (m_env.GetLongestSeg(seg))
+		{
+			// Update skew using a prediction from the convex envelope
+			dfSkew = seg.dfM * dfTXtime + seg.dfC;
+			dfGradient = seg.dfM;
+		}
+
+		// We'll push the result through a filter to ensure that 
+		// skew values don't change too quickly
+		if (m_bIsRunning)
+		{
+			double dt = dfTXtime - m_dfLastTime;
+			double dfFiltOut = SmoothingFilter(dt, m_dfLastVal, dfSkew, dfGradient);
+			dfSkew = dfFiltOut;
+		}
+
+		if (m_env.GetNumSegs() > 500)
+		{
+			// It's highly unlikely ever to fill up this much but just in case...
+			double dfMaxPeriod_hrs = 3.0;
+			double dfMaxPeriod_s   = dfMaxPeriod_hrs * 60.0*60.0;
+			m_env.CropFrontBefore(dfTXtime - dfMaxPeriod_s);
+		}
+
+		m_dfLastVal  = dfSkew;
+		m_dfLastTime = dfTXtime;
+		m_bIsRunning = true;
+
+		return dfSkew;
+	}
+
+
+	CConvexEnvelope::CConvexEnvelope()
+	{
+		Reset();
+	}
+
+	void CConvexEnvelope::Reset()
+	{
+		m_segs.clear();
+		m_bHaveInitPt     =  false;
+		m_InitPt          =  tPt(0,0);
+		m_uiLongestSegID   = -1;
+		m_dfLongestSegLen =  0;
+	}
+
+
+	bool CConvexEnvelope::GetLongestSeg(tSeg &seg) const
+	{
+		if (m_uiLongestSegID >= GetNumSegs()) return false;
+		seg = m_segs[m_uiLongestSegID];
+		return true;
+	}
+
+
+
+	bool CConvexEnvelope::AddPoint(double x, double y)
+	{
+		tPt pt(x, y);
+
+		if (GetNumSegs() == 0 && !m_bHaveInitPt)
+		{
+			// Store this initial point
+			m_InitPt = pt;
+			m_bHaveInitPt = true;
+			return true;
+		}
+
+		// Add a new segment
+		if (!m_bHaveInitPt)
+			return false;
+
+		tSeg seg;
+		if (!MakeSeg(seg, m_InitPt, pt)) return false;
+		m_segs.push_back(seg);
+
+
+		// Now try to merge segments from the back
+		bool bMerged = true;
+		while (GetNumSegs()>1 && bMerged)
+		{
+			bMerged = MergeLastSeg();
+		}	
+
+		// Store latest point for use next time
+		// a point is added
+		m_InitPt = pt;
+
+		return true;
+
+	}
+
+	
+	void CConvexEnvelope::CropFrontBefore(double x_min)
+	{
+		while (GetNumSegs() > 0 && m_uiLongestSegID > 0)
+		{
+			if (m_segs[0].p2.x < x_min)
+			{
+				m_segs.pop_front();
+				m_uiLongestSegID -= 1;
+			}
+		}
+	}
+
+
+	bool CConvexEnvelope::MakeSeg(tSeg &seg, const tPt &p1, const tPt &p2) const
+	{
+		double M=0, C=0;
+
+		if (!GetLineParams(p1, p2, M, C)) return false;
+		
+		tSeg s;
+		s.p1 = p1;
+		s.p2 = p2;
+		s.dfM = M;
+		s.dfC = C;
+		s.dfPeriod = fabs(p2.x - p1.x);
+
+		seg = s;
+		return true;
+	}
+
+	bool CConvexEnvelope::GetLineParams(const tPt &p1, const tPt &p2, double &M, double &C) const
+	{
+		if (p1.x == p2.x) return false;
+
+		M = (p2.y-p1.y) / (p2.x - p1.x);
+		C = p2.y - M*p2.x;
+
+		return true;
+	}
+
+
+	bool CConvexEnvelope::MergeLastSeg()
+	{
+		// Try to merge the latest segment with the one before
+
+		if (GetNumSegs() < 2) return false;
+
+		// Get merge candidates
+		tSeg seg1 = *(m_segs.rbegin()+1);
+		tSeg seg2 = *(m_segs.rbegin());
+
+		if (seg2.dfM < seg1.dfM) return false;
+
+
+		// Later segment is less steep, so a merge is possible
+
+		if (m_uiLongestSegID >= GetNumSegs()-1)
+		{
+			// Ensure we check again for max segment length
+			m_dfLongestSegLen = 0;
+		}
+
+		tSeg mergedSeg;
+		if (!MakeSeg(mergedSeg, seg1.p1, seg2.p2))
+		{
+			return false;
+		}
+
+		mergedSeg.dfPeriod = seg1.dfPeriod + seg2.dfPeriod;
+
+		m_segs.pop_back();
+		m_segs.pop_back();
+		m_segs.push_back(mergedSeg);
+
+		if (mergedSeg.dfPeriod > m_dfLongestSegLen)
+		{
+			m_uiLongestSegID = GetNumSegs()-1;
+		}
+
+		return true;
+
+	}
+
+}
+
+
---
+++ Core/MOOSLIB/MOOSSkewFilter.h
@@ -0,0 +1,124 @@
+///////////////////////////////////////////////////////////////////////////
+//
+//   MOOS - Mission Oriented Operating Suite
+//
+//   A suit of Applications and Libraries for Mobile Robotics Research
+//   Copyright (C) 2001-2005 Massachusetts Institute of Technology and
+//   Oxford University.
+//
+//   This software was written by Paul Newman at MIT 2001-2002 and Oxford
+//   University 2003-2005. email: pnewman@robots.ox.ac.uk.
+//
+//   This file is part of a  MOOS Core Component.
+//
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public License as
+//   published by the Free Software Foundation; either version 2 of the
+//   License, or (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//   General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software
+//   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//   02111-1307, USA.
+//
+//////////////////////////    END_GPL    //////////////////////////////////
+/*! \file MOOSSkewFilter.h */
+
+#ifndef __MOOSSkewFilter_h
+#define __MOOSSkewFilter_h
+
+#include <deque>
+
+namespace MOOS
+{
+	class CConvexEnvelope
+	{
+	public:
+		struct tPt
+		{
+			tPt() : x(0), y(0) {}
+			tPt(double _x, double _y) : x(_x), y(_y) {}
+			double x,y;
+		};
+
+		struct tSeg
+		{
+			double dfPeriod;
+			double dfM, dfC;     // Slope and intersect
+			tPt    p1, p2; // Anchor points through which line passes
+		};
+
+
+	public:
+
+		CConvexEnvelope();
+
+		void Reset();
+		bool AddPoint(double x, double y);
+
+		unsigned int GetNumSegs() const { return m_segs.size(); }
+
+		bool GetLongestSeg(tSeg &seg) const;
+
+		/// Removes all segments from the front which have
+		/// x vals less than x_min.  Will not remove the longest
+		/// segment
+		void CropFrontBefore(double x_min);
+
+	private:
+		bool MergeLastSeg();
+
+		bool MakeSeg(tSeg &seg, const tPt &p1, const tPt &p2) const;
+		bool GetLineParams(const tPt &p1, const tPt &p2, double &M, double &C) const;
+
+	private:
+		// Copy constructor and equality are disabled
+		CConvexEnvelope(const CConvexEnvelope &v) {};
+		const CConvexEnvelope &CConvexEnvelope::operator=(const CConvexEnvelope &v) {};
+
+	private:
+		std::deque<tSeg> m_segs;
+
+		// Temporarily store first point for each pending segment
+		bool m_bHaveInitPt;
+		tPt  m_InitPt;
+
+		unsigned int m_uiLongestSegID;
+		double m_dfLongestSegLen;
+	};
+
+
+	class CMOOSSkewFilter
+	{
+	public:
+		CMOOSSkewFilter();
+
+		void   Reset();
+
+		double Update(double dfTXtime, double dfRXtime, double dfTransportDelay);
+		double GetSkew();
+
+	private:
+		CMOOSSkewFilter(const CMOOSSkewFilter &v);
+		const CMOOSSkewFilter &CMOOSSkewFilter::operator=(const CMOOSSkewFilter &v);
+
+	private:
+		double SmoothingFilter(double dfDT, double dfOldFilterVal, double dfNewMeas, double dfGradient) const;
+
+	private:
+		CConvexEnvelope m_env;
+		bool m_bIsRunning;
+		double m_dfLastVal;
+		double m_dfLastTime;
+	};
+
+}
+
+#endif __MOOSSkewFilter_h
+
+
