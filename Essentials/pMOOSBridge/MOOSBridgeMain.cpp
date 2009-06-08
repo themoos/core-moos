0e9617f554067547170bd4320fc0a68ab3fa1eaa
pnewman <pnewman@5f621d73-a7ea-4b79-bc62-7e07601040e3>
2009-06-08 21:20:00 +0000

    oops - removing testing code
    git-svn-id: svn+ssh://login2/data/avl11/svnroot/MOOS/trunk@2244 5f621d73-a7ea-4b79-bc62-7e07601040e3

--- Essentials/pMOOSBridge/MOOSBridgeMain.cpp
+++ Essentials/pMOOSBridge/MOOSBridgeMain.cpp
@@ -41,10 +41,6 @@ int main(int argc,char* argv[])
     const char * sMissionFile = "Mission.moos";
     const char * sMOOSName = "pMOOSBridge";
     
-	std::string s = "Name=MedianResidualSquared,ID=5,min=0.000062,max=0.084717";
-
-    std::string sStr;
-    MOOSValFromString(sStr, s, "ID", true);
 
     switch(argc)
     {
