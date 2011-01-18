09640487d869d9fbde3e1002a84733162b5bc838
pnewman <pnewman@5f621d73-a7ea-4b79-bc62-7e07601040e3>
2011-01-18 09:43:28 +0000

    oops
    git-svn-id: svn+ssh://login2/data/avl11/svnroot/MOOS/trunk@2357 5f621d73-a7ea-4b79-bc62-7e07601040e3

---
+++ Tools/Console/uPoke/main.cpp
@@ -0,0 +1,113 @@
+
+
+
+
+#include <MOOSLIB/MOOSCommClient.h>
+
+CMOOSCommClient theMOOS;
+
+
+bool OnConnectToServer(void * pParam)
+{
+    return true;
+}
+
+void PrintHelp()
+{
+    MOOSTrace("uPoke Help:\n");
+    MOOSTrace("\n general use :\n\t\"uPoke host portnumber msgName msgContent\"");
+    MOOSTrace("\n use assumed MOOSDB settings (localhost 9000):\n\t\"uPoke -d msgName msgContent\"");
+    MOOSTrace("\n\n\texamples:\n");
+    MOOSTrace("\t   uPoke robots.ox.ac.uk 9010 BeSmart how=think,when=now\n");
+    MOOSTrace("\t   uPoke robots.ox.ac.uk 9010 WorryLevel 0.99\n");
+    MOOSTrace("\t   uPoke -d WorryLevel 0.99\n");
+
+}
+
+bool Send(CMOOSMsg  Msg)
+{
+    while(!theMOOS.IsConnected())
+        MOOSPause(10);
+
+
+    if(theMOOS.Post(Msg))
+    {
+        theMOOS.Register(Msg.GetKey(),0);
+
+        MOOSMSG_LIST M;
+        while(!theMOOS.Fetch(M))
+        {
+            MOOSPause(10);
+        }
+    }
+
+    return true;
+}
+
+int main(int argc, char * argv[])
+{
+    //before we do anything....
+    theMOOS.SetQuiet(true);
+    theMOOS.SetOnConnectCallBack(OnConnectToServer,NULL);
+
+
+
+    std::string sServer = "localhost";
+    int nPort = 9000;
+    std::string sName;
+
+    std::vector<std::string> Arguments;
+
+    for (unsigned int i = 1; i < argc; i++)
+    {
+        Arguments.push_back(argv[i]);
+    }
+
+
+    //uPoke -d msg content
+    //uPoke localhost 9000 msg contents
+
+    unsigned int k = 0;
+    if (Arguments.size() == 3)
+    {
+        if (Arguments[k++] != "-d")
+        {
+            PrintHelp();
+            exit(1);
+        }
+
+    }
+    else if (Arguments.size() == 4)
+    {
+
+        sServer = Arguments[k++];
+
+        if (!MOOSIsNumeric(Arguments[k]))
+            return MOOSFail("second argument (port number) is not numeric");
+
+        nPort = atoi(Arguments[k++].c_str());
+
+
+    }
+
+
+    theMOOS.Run(sServer.c_str(), nPort, theMOOS.GetLocalIPAddress().c_str(),40);
+
+    sName = Arguments[k++];
+
+    bool bOK;
+    if (MOOSIsNumeric(Arguments[k]))
+    {
+        bOK = Send(CMOOSMsg(MOOS_NOTIFY, sName, atof(Arguments[k].c_str()),
+                MOOSTime()));
+    }
+    else
+    {
+        bOK =Send(CMOOSMsg(MOOS_NOTIFY, sName, Arguments[k].c_str(), MOOSTime()));
+    }
+
+    return bOK ? 0: 1;
+
+}
+
+
