88b746baa6d9018ac5cd6b249b05b301d7d8805a
pnewman <pnewman@5f621d73-a7ea-4b79-bc62-7e07601040e3>
2011-01-24 17:53:01 +0000

    PMN adds MOOSBinaryComms example
    git-svn-id: svn+ssh://login2/data/avl11/svnroot/MOOS/trunk@2361 5f621d73-a7ea-4b79-bc62-7e07601040e3

---
+++ Docs/ProgrammingWithMOOS/code/MOOSBinaryMessaging/main.cpp
@@ -0,0 +1,79 @@
+
+#include <MOOSLIB/MOOSApp.h>
+
+#include <iostream>
+
+class CMessager: public CMOOSApp
+{
+public:
+    CMessager():_nSize(-1),_bPublisher(false){};
+
+
+    bool OnNewMail(MOOSMSG_LIST & Mail)
+    {
+
+        for(MOOSMSG_LIST::iterator q = Mail.begin();q!=Mail.end();q++)
+            q->Trace();
+        return true;
+    }
+    bool Iterate()
+    {
+        if(_bPublisher &&_nSize>0)
+        {
+            std::vector<char> VastMessage(_nSize);
+            m_Comms.Notify("BigThing",(VastMessage.data()),_nSize);
+        }
+
+        return true;
+    }
+    bool OnConnectToServer()
+    {
+        if(!_bPublisher)
+            m_Comms.Register("BigThing",0);
+
+        return true;
+    }
+
+    bool OnStartup()
+   {
+       SetAppFreq(1);
+       SetCommsFreq(20);
+
+       if(!_bPublisher)
+           m_Comms.Register("BigThing",0);
+   }
+
+
+
+    void MakePublisher(int nSize)
+    {
+        _nSize = nSize;
+        _bPublisher = true;
+        std::cout<<"will publish "<<_nSize<<" bytes"<<std::endl;
+    }
+
+protected:
+    int _nSize;
+    bool _bPublisher;
+};
+
+
+int main(int argc, char * argv[])
+{
+    CMessager M;
+
+    std::string sMyName = "binary_tester";
+
+    if(argc>1)
+    {
+        M.MakePublisher(atoi(argv[1]) );
+        sMyName+="_publisher";
+    }
+    else
+    {
+        sMyName+="_consumer";
+        std::cout<<"I will subscribed to BigThing\n";
+    }
+
+    M.Run(sMyName.c_str(),"mission.moos");
+}
