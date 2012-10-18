8dc3b918b193b8cb35239e91d3bb567776734ce3
Paul Newman <pnewman@robots.ox.ac.uk>
2012-10-18 09:52:53 +0100

    allowed KeyboardCapture class a chance to see if its std::cin is actually a terminal
    This prevents a 100% CPU loading which happened using some of mikerb's launch scripts which backgrounded pAntler which its elf spawned MOOS processes.
    
    Signed-off-by: Paul Newman <pnewman@robots.ox.ac.uk>

--- Core/libMOOS/Utils/KeyboardCapture.cpp
+++ Core/libMOOS/Utils/KeyboardCapture.cpp
@@ -2,7 +2,7 @@
 #include "MOOS/libMOOS/Utils/SafeList.h"
 #include "MOOS/libMOOS/Utils/KeyboardCapture.h"
 #include <iostream>
-//#include "unistd.h"
+//#include <fstream>
 
 namespace MOOS
 {
@@ -26,10 +26,20 @@ bool KeyboardCapture::dispatch(void * param)
 }
 bool KeyboardCapture::Capture()
 {
+	/*
+	std::ofstream iodebug("iodebug.txt");
 //	MOOSPause(1000);
-//	std::cout<<"cin :"<< isatty(0)<<std::endl;
-//	std::cout<<"cout :"<< isatty(1)<<std::endl;
-//	std::cout<<"cerr :"<< isatty(2)<<std::endl;
+	iodebug<<"cin :"<< isatty(0)<<std::endl;
+	iodebug<<"cout :"<< isatty(1)<<std::endl;
+	iodebug<<"cerr :"<< isatty(2)<<std::endl;
+	*/
+
+	if(isatty(0)==0)
+	{
+		std::cerr<<"KeyboardCapture::Capture std::cin is not a tty. Thread exiting.\n";
+		return false;
+	}
+
 	while(!impl_->worker_.IsQuitRequested())
 	{
 		char c = MOOSGetch();
