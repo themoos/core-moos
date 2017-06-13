0289b4f9967e4defb4e702d10a3631bb8f97f3a8
Alastair Harrison <aharrison24@gmail.com>
2017-06-13 16:46:15 +0100

    Remove unused macports support
    - There are no find_library calls, so setting CMAKE_LIBRARY_PATH and
      CMAKE_INCLUDE_PATH does nothing. In any case they are harmful
      because they pollute the search paths.

--- cmake/PlatformDefines.cmake
+++ cmake/PlatformDefines.cmake
@@ -8,12 +8,6 @@ if(UNIX)
     add_definitions(-DUNIX)
     if("${CMAKE_SYSTEM_NAME}" MATCHES "Darwin")
         add_definitions( -DPLATFORM_DARWIN )
-
-        #some support for macport installed libraries
-        set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} /opt/local/lib)
-        set(CMAKE_INCLUDE_PATH ${CMAKE_INCLUDE_PATH} /opt/local/include)
-        message(STATUS "Cmake library path ${CMAKE_LIBRARY_PATH}")
-
         set( PLATFORM_DARWIN 1 )
     endif()
     if("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
