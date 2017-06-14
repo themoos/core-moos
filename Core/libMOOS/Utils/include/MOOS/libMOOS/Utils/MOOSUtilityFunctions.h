4ac7c12940baa77c94ecd1019b4f3173e4d27703
Alastair Harrison <alastair@oxbotica.com>
2017-06-14 13:36:39 +0100

    Fix UNUSED_PARAMETER macro
    - It wasn't doing anything for non-windows platforms.

--- Core/libMOOS/Utils/include/MOOS/libMOOS/Utils/MOOSUtilityFunctions.h
+++ Core/libMOOS/Utils/include/MOOS/libMOOS/Utils/MOOSUtilityFunctions.h
@@ -311,13 +311,7 @@ std::string & MOOSAddValToString(std::string & sIn, const std::string & sTok,con
 
 
 ///////// USEFUL MACRO FOR PREVENTING COMPILER WARNINGS
-#ifdef _WIN32
-#define UNUSED_PARAMETER(a) a
-#else
-#define UNUSED_PARAMETER(a)
-#endif
-
-
+#define UNUSED_PARAMETER(arg) ((void)&(arg))
 
 #endif
 
