486f5d109167c167460a8f6dc98e446e3ea3a642
Paul Newman <pnewman@Pauls-MacBook-Pro-2.local>
2017-12-21 14:56:59 +0000

    printing bandwidth on a per channel basis in atm
    sorted by value

--- Core/tools/atm/atm.cpp
+++ Core/tools/atm/atm.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <string>
+#include <map>
+
 #include <cstring>
 #include <stdexcept>
 #include <iomanip>
@@ -26,6 +28,90 @@ const std::string kDefaultAuditDirectory = "moos_comms_audits";
 std::string multicast_address = "224.1.1.8";
 int multicast_port = 4000;
 std::string name_pattern;
+double kBandwidthFIRConstant = 0.01;
+
+
+struct atm_stat{
+    double average_bandwidth;
+    std::string source;
+    std::string destination;
+    std::string name;
+    int64_t last_write_time;
+};
+std::map<std::string, atm_stat> atm_stats;
+
+bool sort_on_bw(const atm_stat & a, const atm_stat &b){
+    return a.average_bandwidth>b.average_bandwidth;
+}
+
+
+std::string FormatBytes(int n) {
+    std::stringstream ss;
+    if (n == 0) {
+        ss << n;
+    } else if (n > (1 << 30)) {
+        ss << (n >> 30) << " GB/s";
+    } else if (n > (1 << 20)) {
+        ss << (n >> 20) << " MB/s";
+    } else if (n > (1 << 10)) {
+        ss << (n >> 10) << " kB/s";
+    } else {
+        ss << " B/s";
+    }
+
+    return ss.str();
+}
+
+
+void PrintStatistics(){
+    std::cerr<<"bandwidth by channel\n";
+
+    std::list<atm_stat> stat_list;
+    std::map<std::string ,atm_stat>::iterator p;
+    for(p=atm_stats.begin();p!=atm_stats.end();p++){
+        stat_list.push_back(p->second);
+    }
+
+    stat_list.sort(sort_on_bw);
+
+    std::list<atm_stat>::iterator q;
+    for(q=stat_list.begin();q!=stat_list.end();q++){
+        std::stringstream ss;
+
+        ss<<std::setw(8)<<FormatBytes(int(q->average_bandwidth))<<" ";
+        ss<<std::setw(15)<<"\""<<q->name<<"\""
+         <<" from "<<q->source
+         <<" to "<<q->destination<<"\n";
+        std::cerr<<ss.str();
+    }
+}
+
+void AccummulateStatistic(MOOS::EndToEndAudit::MessageStatistic & ms){
+    std::string key= ms.message_name+":"+ms.source_client+":"+ms.destination_client;
+    std::map<std::string, atm_stat>::iterator q;
+    q = atm_stats.find(key);
+    if(q==atm_stats.end()){
+        atm_stat new_statistic;
+        new_statistic.name = ms.message_name;
+        new_statistic.destination = ms.destination_client;
+        new_statistic.source = ms.source_client;
+        new_statistic.average_bandwidth = 0.0;
+        new_statistic.last_write_time = ms.receive_time;
+        atm_stats[key] = new_statistic;
+    }else{
+        atm_stat & stat=q->second;
+        int64_t dt = ms.receive_time-stat.last_write_time+1;
+        double point_estimate_bandwidth = (1e6*ms.message_size)/dt;
+        stat.last_write_time = ms.receive_time;
+        stat.average_bandwidth = (1.0-kBandwidthFIRConstant)*stat.average_bandwidth+
+                kBandwidthFIRConstant*point_estimate_bandwidth;
+    }
+
+
+}
+
+
+
 void PrintHelpAndExit()
 {
     std::cerr<<"audit the MOOS (atm)\n";
@@ -200,15 +286,19 @@ int main(int argc, char *argv[])
                     PrettyPrint(std::cerr,ms);
                 }
             }
+
+            AccummulateStatistic(ms);
         }
 
-        if(MOOSTime()-last_display_time>10.0){
+        if(MOOSTime()-last_display_time>3.0){
             last_display_time = MOOSTime();
             std::cerr<<MOOSFormat("processed %.8d messages and logged %.6d. "
                                   "Mean delay %.2fuS\n",
                                   number_messages_rxed,
                                   number_messages_logged,
                                   average_latency_us);
+
+            PrintStatistics();
         }
 
 
